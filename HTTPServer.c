d9d55ada6e2f0b052701a6b1d7eb8a4976f0b1e7
thaisartell <thaisartell@gmail.com>
2026-03-15 15:13:12 -0500

    before i pivot to digikey code

--- HTTPServer.c
+++ HTTPServer.c
@@ -8,8 +8,13 @@
 #include "lwip/ip4_addr.h"
 #include "lwip/tcp.h"
 
-static const char WIFI_SSID[] = "CenturyLink2405";
-static const char WIFI_PASSWORD[] = "a7df7b3f6eeed6";
+static const char WIFI_SSID[] = "nasPico";
+static const char WIFI_PASSWORD[] = "LuBrRaJaTh";
+
+#define WIFI_CHANNEL 1
+#define CYW43_AUTH_WPA2_AES_PSK (0x00400004)
+
+cyw43_t cyw43_state;
 
 
 int main() {
@@ -18,16 +23,15 @@ int main() {
         printf("failed to initialise\n");
         return 1;
     }
-    // if (cyw43_arch_init_with_country(CYW43_COUNTRY_USA)) {
-    //     printf("failed to initialise\n");
-    //     return 1;
-    // }
-    cyw43_arch_enable_sta_mode();
 
-    // Connect to the WiFI network - loop until connected
-    while(cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000) != 0){
-        printf("Attempting to connect...\n");
-    }
+    /* Enable CYW43 architecture AP mode */
+    cyw43_arch_enable_ap_mode(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK);
+    
+    /* Set CYW43 access point communication channel */
+    cyw43_wifi_ap_set_channel(&cyw43_state, WIFI_CHANNEL);
+
+    lwip_init();
+
     // Print a success message once connected
     printf("Connected! \n");
     
@@ -37,83 +41,10 @@ int main() {
 
     printf("connected\n");
     
-    while(1);
+    pico_cyw43_arch_lwip_poll();
+    while(1) {
+        cyw43_arch_poll();
+    }
 
     return 1;
 }
-
-
-/*
-extern struct netif *netif_default;
-
-static err_t on_sent(void *arg, struct tcp_pcb *tpcb, u16_t len) {
-    tcp_close(tpcb);
-    return ERR_OK;
-}
-
-static void heartbeat(void) {
-    static bool on = false;
-    on = !on;
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
-}
-
-static err_t on_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err) {
-    if (!p) { tcp_close(tpcb); return ERR_OK; }   // client closed
-    tcp_recved(tpcb, p->tot_len);
-    pbuf_free(p);
-
-    static const char resp[] =
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Type: text/plain\r\n"
-        "Connection: close\r\n"
-        "\r\n"
-        "Hello from Pico\r\n";
-
-    tcp_write(tpcb, resp, sizeof(resp) - 1, TCP_WRITE_FLAG_COPY);
-    tcp_output(tpcb);
-    return ERR_OK;
-}
-
-static err_t on_accept(void *arg, struct tcp_pcb *newpcb, err_t err) {
-    tcp_recv(newpcb, on_recv);
-    tcp_sent(newpcb, on_sent);
-    return ERR_OK;
-}
-
-static void httpd_raw_init(void) {
-    struct tcp_pcb *pcb = tcp_new_ip_type(IPADDR_TYPE_V4);
-    tcp_bind(pcb, IP4_ADDR_ANY, 80);
-    pcb = tcp_listen_with_backlog(pcb, 4);
-    tcp_accept(pcb, on_accept);
-}
-
-int main() {
-    stdio_init_all();
-    sleep_ms(2000);
-
-    if (cyw43_arch_init_with_country(CYW43_COUNTRY_USA)) {
-        printf("cyw43 init failed\n");
-        return 1;
-    }
-    cyw43_arch_enable_sta_mode();
-
-    while (cyw43_arch_wifi_connect_timeout_ms(
-               WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000) != 0) {
-        printf("Attempting to connect...\n");
-    }
-
-    if (netif_default) {
-        printf("IP: %s\n", ip4addr_ntoa(netif_ip4_addr(netif_default)));
-    }
-
-    cyw43_arch_lwip_begin();
-    httpd_raw_init();
-    cyw43_arch_lwip_end();
-
-    while (true) {
-        heartbeat();
-        tight_loop_contents();
-        sleep_ms(250);
-    }
-}
-*/
---
+++ http.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+#define PICO_SSID "smartNAS"
